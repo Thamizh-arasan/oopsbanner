9ed44eee3c29163dfe7aea2b99e7c15608234ccb
Thamizh-arasan <thamizharasanvbye@gmail.com>
2026-04-10 13:41:50 +0530

    Update UC2.java

--- UC2.java
+++ UC2.java
@@ -1,7 +1,7 @@
 class OOPSBannerApp2 {
     public static void main(String[] args) {
 
-        System.out.println("      ***      ***      *****       *****      ");
+        System.out.println("     https://github.com/Thamizh-arasan/hello-appp.git     ");
         System.out.println("     ** **    ** **    **   **    **           ");
         System.out.println("    **   **  **   **   **   **   **            ");
         System.out.println("    **   **  **   **   *****      ***          ");
